63967a761144fcc129399b287dd8d674aa577be7
m-e-h <info@martyhelmick.com>
2015-02-12 13:08:43 -0500

    correct text-domain, removed unused custom bg stuff

--- functions.php
+++ functions.php
@@ -1,25 +1,28 @@
 <?php
 /**
- * This program is free software; you can redistribute it and/or modify it under the terms of the GNU 
- * General Public License as published by the Free Software Foundation; either version 2 of the License, 
+ * This program is free software; you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License as published by the Free Software Foundation; either version 2 of the License,
  * or (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without 
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
  * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  *
- * You should have received a copy of the GNU General Public License along with this program; if not, write 
+ * You should have received a copy of the GNU General Public License along with this program; if not, write
  * to the Free Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
  */
+
 /* Add the child theme setup function to the 'after_setup_theme' hook. */
 add_action( 'after_setup_theme', 'material_gaze_theme_setup' );
+
 /**
- * Setup function.  All child themes should run their setup within this function.  The idea is to add/remove 
+ * Setup function.  All child themes should run their setup within this function.  The idea is to add/remove
  * filters and actions after the parent theme has been set up.  This function provides you that opportunity.
  *
  * @since  1.0.0
  * @access public
  * @return void
  */
+
 function material_gaze_theme_setup() {
 	/* Add a custom background to overwrite the defaults. */
 	add_theme_support(
@@ -29,16 +32,18 @@ function material_gaze_theme_setup() {
 			'default-image' => '',
 		)
 	);
+
 	/* Add a custom header to overwrite the defaults. */
-	add_theme_support( 
-		'custom-header', 
+	add_theme_support(
+		'custom-header',
 		array(
 			'default-text-color' => 'ffffff',
 			'default-image'      => '%2$s/images/headers/blue.jpg',
 			'random-default'     => false,
-			'wp-head-callback'   => 'mg_custom_header_wp_head',
+			'wp-head-callback'   => 'material_gaze_custom_header_wp_head',
 		)
 	);
+
 	/* Register default headers. */
 	register_default_headers(
 		array(
@@ -54,6 +59,7 @@ function material_gaze_theme_setup() {
 			),
 		)
 	);
+
 	/* Custom editor stylesheet. */
 	add_editor_style( '//fonts.googleapis.com/css?family=Roboto:400,300,300italic,400italic,500,700|Roboto+Condensed:400,300,700' );
 	/* Filter to add custom default backgrounds (supported by the framework). */
@@ -63,27 +69,7 @@ function material_gaze_theme_setup() {
 	/* Load stylesheets. */
 	add_action( 'wp_enqueue_scripts', 'material_gaze_enqueue_styles', 0 );
 }
-/**
- * Registers custom default backgrounds.
- *
- * @since  1.0.0
- * @access public
- * @param  array  $backgrounds
- * @return array
- */
-function material_gaze_default_backgrounds( $backgrounds ) {
-	$new_backgrounds = array(
-		'gray-square' => array(
-			'url'           => '%2$s/images/backgrounds/gray-square.png',
-			'thumbnail_url' => '%2$s/images/backgrounds/gray-square-thumb.png',
-		),
-		'blue-square' => array(
-			'url'           => '%2$s/images/backgrounds/blue-square.png',
-			'thumbnail_url' => '%2$s/images/backgrounds/blue-square-thumb.png',
-		),
-	);
-	return array_merge( $new_backgrounds, $backgrounds );
-}
+
 /**
  * Add a default custom color for the theme's "primary" color option.
  *
@@ -95,9 +81,11 @@ function material_gaze_default_backgrounds( $backgrounds ) {
 function material_gaze_color_primary( $hex ) {
 	return $hex ? $hex : '2196F3';
 }
+
 function material_gaze_enqueue_styles() {
 	wp_enqueue_style( 'material-gaze-fonts', '//fonts.googleapis.com/css?family=Roboto:400,300,300italic,400italic,500,700|Roboto+Condensed:400,300,700' );
 }
+
 add_action( 'wp_head', 'material_gaze_wp_head' );
 function material_gaze_wp_head() {
 	$style = '';
@@ -107,7 +95,8 @@ function material_gaze_wp_head() {
 textarea:focus, select:focus { border-bottom-color: #{$hex}; box-shadow: 0 1px 0 0 #{$hex}; } ";
 	echo "\n" . '<style type="text/css">' . trim( $style ) . '</style>' . "\n";
 }
-function mg_custom_header_wp_head() {
+
+function material_gaze_custom_header_wp_head() {
 	if ( !display_header_text() )
 		return;
 	$hex = get_header_textcolor();
@@ -116,10 +105,12 @@ function mg_custom_header_wp_head() {
 	$style = "body.custom-header #branding, #site-title { color: #{$hex}; }";
 	echo "\n" . '<style type="text/css" id="custom-header-css">' . trim( $style ) . '</style>' . "\n";
 }
+
 /**
  * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
  */
-function mg_customize_preview_js() {
-	wp_enqueue_script( 'mg_customizer', trailingslashit( CHILD_THEME_URI ) .  'js/customizer.js', array( 'customize-preview' ), '20130508', true );
+function material_gaze_customize_preview_js() {
+	wp_enqueue_script( 'material_gaze_customizer', trailingslashit( CHILD_THEME_URI ) .  'js/customizer.js', array( 'customize-preview' ), '20130508', true );
 }
-add_action( 'customize_preview_init', 'mg_customize_preview_js' );
+
+add_action( 'customize_preview_init', 'material_gaze_customize_preview_js' );
